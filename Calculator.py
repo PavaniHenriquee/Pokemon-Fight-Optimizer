79490af7b2bafcfdc03a3a1c6e25c8cd5f92fdcf
Henrique <henriquepavanisilva@gmail.com>
2025-08-27 19:51:52 -0300

    Test

--- Calculator.py
+++ Calculator.py
@@ -2,15 +2,27 @@ import json
 #import numpy
 
 round = 1
-pokemon = {my:{}, opp:{}}
-currentHP = {my:0, opp:0}
-with open("PkDB.json","r") as f:
-    pkBD = json.load(f)
-print(pkDB["Bulbasaur"]["type"])
-
+myPartyPk = ['Charmander']
+oppPartyPk = ['Squirtle']
+with open("Database\PkDB.json","r") as f:
+    pkDB = json.load(f)
+with open("Database\AbilitiesDB.json","r") as f:
+    abDB = json.load(f)
+with open("Database\ItemDB.json","r") as f:
+    itemDB = json.load(f)
+with open("Database\MoveDB.json","r") as f:
+    moveDB = json.load(f)
+myParty = {pkDB[myPartyPk[0]]:{
+            'ability': abDB['Blaze'],
+            'move1': moveDB['Tackle'],
+            'move2': moveDB['Growl']}
+            }
+oppParty = {pkDB[oppPartyPk[0]]:{
+            'ability': abDB['Blaze'],
+            'move1': moveDB['Tackle'],
+            'move2': moveDB['Growl']}
+            }
 def battleStart():
     myPokemon = {}
     oppPokemon = {}
-    currentHP = myPokemon[HP]
-    currentOppHp = oppPokemon[HP]
 
