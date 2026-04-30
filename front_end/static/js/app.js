2dc3d78fefb5207b534f46536276a3d413ea05f4
Henrique <henriquepavanisilva@gmail.com>
2026-04-30 14:15:18 -0300

    Restarting frontend so i can do it with react

--- front_end/static/js/app.js
+++
@@ -1,8 +0,0 @@
-let popupBox = document.getElementById("popup_box");
-
-function openPopupBox(){
-    popupBox.classList.add("open-popup_box");
-}
-function closePopupBox(){
-    popupBox.classList.remove("open-popup_box");
-}
